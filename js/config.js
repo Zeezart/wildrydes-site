64a165686ceefc253268bf97220eb569ef23dce8
Zeezart <109485000+Zeezart@users.noreply.github.com>
2025-09-26 17:49:40 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_Lm4QsBvyr', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5ud9unrlij2nleom16obu848v3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '22egnpboejidu3a4g7t4en57jb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
